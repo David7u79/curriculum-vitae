c8df16d7a3edd332e93058d3ba36985d770f90ff
David7u79 <pokentrenadorgamerdbl@gmail.com>
2026-07-04 12:25:31 -0600

    feat: enhance footer with styled builder link and update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://David7u79.github.io',
   base: '/curriculum-vitae',
+  devToolbar: { enabled: false },
 });
